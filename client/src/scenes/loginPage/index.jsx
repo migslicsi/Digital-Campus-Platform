b3db24b200b34bd7942aff76f48804288a889777
migslicsi <106062492+migslicsi@users.noreply.github.com>
2023-03-17 14:18:01 +0800

    v1.3 reverted login form changes

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -2,47 +2,43 @@ import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form.jsx";
 
 const LoginPage = () => {
-  const theme = useTheme();
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-
-  return (
-    <Box
-      sx={{
-        background: `linear-gradient(to bottom, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backdropFilter: "blur(px) opacity(0.8)", // Add backdrop-filter property here
-      }}
-    >
-      <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        p="2rem"
-        borderRadius="1.5rem"
-        bgcolor="rgba(255, 255, 255, 255)" // Change the background color to transparent with some opacity
-      >
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box 
+        width="100%" 
+        backgroundColor={theme.palette.background.alt} 
+        p="1rem 6%" 
+        textAlign="center"
+        >
         <Typography
-          fontWeight="bold"
-          fontSize="32px"
-          color={theme.palette.primary.main}
-          mb="1.5rem"
-          textAlign="center"
+        fontWeight="bold"
+        fontSize="32px"
+        color="primary"
         >
           CIIT++
         </Typography>
-        <Typography
+        </Box>
+
+        <Box
+          width={isNonMobileScreens ? "50%" : "93%"}
+          p="2rem"
+          m="2rem auto"
+          borderRadius="1.5rem"
+          backgroundColor={theme.palette.background.alt}
+        >
+          <Typography
           fontWeight="500"
           variant="h5"
-          sx={{ mb: "1.5rem" }}
-          color={theme.palette.text.primary}
-        >
-          Welcome to CIIT++, The Exclusive Online Campus Platform for CIIT Students!
-        </Typography>
-        <Form />
-      </Box>
-    </Box>
-  );
+          sx= {{ mb: "1.5rem"}}
+          >
+            Welcome to CIIT++, The Exclusive Online Campus Platform for CIIT Students!
+          </Typography>
+          <Form />
+
+
+        </Box>
+    </Box>;
 };
 
 export default LoginPage;
