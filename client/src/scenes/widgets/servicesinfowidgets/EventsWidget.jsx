339a3df9bd430fa93a99a27a0c87f6e798c023db
migslicsi <106062492+migslicsi@users.noreply.github.com>
2023-03-21 05:35:47 +0800

    added frontend layouts

--- client/src/scenes/widgets/servicesinfowidgets/EventsWidget.jsx
+++ client/src/scenes/widgets/servicesinfowidgets/EventsWidget.jsx
@@ -6,6 +6,9 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import { motion } from "framer-motion";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const EventsWidget = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -44,7 +47,13 @@ const EventsWidget = () => {
 
             <Box m={1} display="flex" sx={{ gap: '1rem' }} flexWrap="wrap">
             {/*event 1*/}
-            <Card sx={{ maxWidth: 345, borderRadius: 3,  }}>
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -53,22 +62,35 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/japan.png"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
-
+            </motion.div>
+            
               {/*event 2*/}
-              <Card sx={{ maxWidth: 345, borderRadius: 3  }}>
+              <motion.div
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.2 },
+              }}
+              >
+              <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -77,22 +99,35 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/sportsfest.png"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
+            </motion.div>
 
             {/*event 3*/}
-            <Card sx={{ maxWidth: 345, borderRadius: 3  }}>
+            <motion.div
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.2 },
+              }}
+              >
+            <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -101,22 +136,35 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/pride.jpg"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
+            </motion.div>
 
             {/*event 4*/}
-            <Card sx={{ maxWidth: 345, borderRadius: 3  }}>
+            <motion.div
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.2 },
+              }}
+              >
+            <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -125,22 +173,35 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/fair.jpg"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
+            </motion.div>
 
             {/*event 5*/}
-            <Card sx={{ maxWidth: 345, borderRadius: 3  }}>
+            <motion.div
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.2 },
+              }}
+              >
+            <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -149,22 +210,35 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/alumni.jpg"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
+            </motion.div>
 
             {/*event 6*/}
-            <Card sx={{ maxWidth: 345, borderRadius: 3  }}>
+            <motion.div
+              whileHover={{
+                scale: 1.05,
+                transition: { duration: 0.2 },
+              }}
+              >
+            <Card sx={{ width: 345, borderRadius: 3, boxShadow: '0 2px 8px rgba(0, 213, 250, 0.6)' }}>
              <CardMedia
                 padding="1rem"
                 component="img"
@@ -173,19 +247,26 @@ const EventsWidget = () => {
                 image="http://localhost:3001/assets/science.jpg"
              />
                <CardContent>
-                <Typography color={dark} gutterBottom variant="h5" component="div">
+                <Typography color={dark} gutterBottom variant="h4" component="div">
                   Japan Foundation Day 2023
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Pellentesque lacinia urna non aliquet vestibulum.
+                <Typography variant="h7" color="text.secondary">
+                  <div style={{ display: "flex", alignItems: "left", justifyContent: "left" }}>
+                    <CalendarMonthIcon /> Thursday, March 10th, 4:00am <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <AttachMoneyIcon /> 40 <br></br>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
+                    <LocationOnIcon /> Mid Lane
+                  </div>
                 </Typography>
                 </CardContent>
                 <CardActions>
-                <Button size="small">Share</Button>
                 <Button size="small">Learn More</Button>
                 </CardActions>
             </Card>
+            </motion.div>
 
 
             </Box>
--- client/src/scenes/widgets/servicesinfowidgets/HousingWidget.jsx
+++ client/src/scenes/widgets/servicesinfowidgets/HousingWidget.jsx
@@ -1,6 +1,11 @@
 import { Divider, useTheme, Box, Typography, useMediaQuery } from "@mui/material";
 import * as React from 'react';
 import { motion } from "framer-motion";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 
 const HousingWidget = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -38,11 +43,287 @@ const HousingWidget = () => {
             </Typography>
 
             <Box m={1} display="flex" sx={{ gap: '1rem' }} flexWrap="wrap">
+
+            <Typography pl="1rem" mt="0.5rem" variant="h6" component="p">
+            Truly a home away from home, it consists of student dormitories and apartments that encourage socio-spiritual nurturing while also supporting physical and academic development. Competent and caring residence deans and assistants cooperate with parents in helping the students realize their potential while doing their studies. Choose from the different dormitories and apartments that will suit your needs.
+            </Typography>
             
-            {/* your content here */}
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house1.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Acacia Residences
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+            
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house8.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Eastern Residence Hall
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house2.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Varsity Residences
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house3.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Arezzo Place
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house4.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Jose Rizal Hall
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house5.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Will Apartments
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house6.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Benaroya Hall
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
+
+            {/* house card */}
+            <motion.div
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.2 },
+            }}
+            >
+            <Card sx={{ height: 350, width: 550, borderRadius: 3,
+            "@media(max-width: 1000px)": {
+              width:"380px"
+            },
+            "@media(max-width: 470px)": {
+              width:"300px",
+              height:"200px"
+            },
+            }}>
+             <CardMedia
+                padding="1rem"
+                component="img"
+                alt="green iguana"
+                height="100%"
+                width="100%"
+                image="http://localhost:3001/assets/house7.jpg"
+             />
+            </Card>
+            <CardContent>
+                <Typography sx={{ fontStyle: 'bold',  letterSpacing: 2  }} align="center" color={dark} gutterBottom variant="h4" component="div">
+                  Furion Residences
+                </Typography>
+                <Typography variant="h7" color="text.secondary">
+                </Typography>
+                </CardContent>
+            </motion.div>
 
             </Box>
           </motion.Box>
+
+          
 
     );
 };
