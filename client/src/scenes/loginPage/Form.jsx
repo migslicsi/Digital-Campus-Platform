8f17250dbdb3bb9ffcfe6c61720b30c8e9246b05
migslicsi <106062492+migslicsi@users.noreply.github.com>
2023-04-22 16:53:34 +0800

    added privacy policy

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -9,6 +9,9 @@ import {
   Select,
   MenuItem,
   InputLabel,
+  Link,
+  Modal,
+  IconButton,
 } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
@@ -18,6 +21,8 @@ import { useDispatch } from "react-redux";
 import { setLogin } from "state";
 import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
+import CloseIcon from "@mui/icons-material/Close";
+
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("First Name Required"),
@@ -105,6 +110,28 @@ const Form = () => {
   const handleFormSubmit = async (values, onSubmitProps) => {
     if (isLogin) await login(values, onSubmitProps);
     if (isRegister) await register(values, onSubmitProps);
+  };
+
+  const [isTermsModalOpen, setIsTermsModalOpen] = useState(false);
+
+  const handleTermsClick = () => {
+    setIsTermsModalOpen(true);
+  };
+
+  const handleTermsClose = () => {
+    setIsTermsModalOpen(false);
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
   };
 
   return (
@@ -270,6 +297,59 @@ const Form = () => {
                 ? "Don't have an account? Sign Up here."
                 : "Already have an account? Login here."}
             </Typography>
+            <br/>
+            <Typography gutterBottom align="center" variant="body2">
+              {isLogin
+                ? "By Logging in, you agree to our "
+                : "By Signing Up, you agree to our "}
+              <Link href="#" onClick={handleTermsClick}>Terms and Privacy Policy.</Link>
+            </Typography>
+            <Modal
+              open={isTermsModalOpen}
+              onClose={handleTermsClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: isNonMobile ? 800 : '90vw',
+                  maxWidth: '90vw',
+                  bgcolor: 'background.paper',
+                  border: '2px solid #000',
+                  boxShadow: 24,
+                  p: 4,
+                  maxHeight: '90vh',
+                  overflow: 'auto'
+                }}>
+                  <IconButton sx={{ position: 'absolute', top: 0, right: 0, color: 'primary.main',}} onClick={handleTermsClose}>
+                    <CloseIcon />
+                  </IconButton>
+                <Typography align="center" id="modal-modal-title" variant="h6" component="h2">
+                  Welcome to CIIT++! <br></br> By accessing or using our platform, you agree to the following terms and conditions:
+                </Typography>
+                <Typography gutterBottom align="left" variant="body2" id="modal-modal-description" sx={{ mt: 2 }}>
+                  1. User Accounts: To access certain features of our platform, you may need to create a user account. When creating an account, you agree to provide accurate and complete information. You are responsible for maintaining the confidentiality of your account login information and for all activities that occur under your account.
+                  <br/><br/>
+                  2. Content: Our platform includes various features, including an exclusive social network and school information/service hubs, where you can post, upload, or share content. You are solely responsible for any content that you post, upload, or share on our platform. By posting, uploading, or sharing content, you grant us a non-exclusive, transferable, sub-licensable, royalty-free, worldwide license to use, copy, modify, create derivative works based on, distribute, publicly display, publicly perform, and otherwise exploit in any manner such content in all formats and distribution channels now known or hereafter devised.
+                  <br/><br/>
+                  3. Prohibited Conduct: You agree not to use our platform for any unlawful or prohibited purpose or to engage in any conduct that could damage, disable, or impair the operation of our platform. Prohibited conduct includes, but is not limited to, the following: posting, uploading, or sharing any content that is defamatory, obscene, pornographic, or otherwise objectionable; using our platform to harass, stalk, or threaten others; using our platform to engage in any activity that violates the intellectual property rights of others.
+                  <br/><br/>
+                  4. Privacy: We take the privacy of our users seriously. Please review our Privacy Policy to learn more about how we collect, use, and disclose information about our users.
+                  <br/><br/>
+                  5. Modifications: We reserve the right to modify or update these terms and conditions at any time without prior notice. Your continued use of our platform after such modifications will constitute your agreement to the updated terms and conditions.
+                  <br/><br/>
+                  6. Termination: We reserve the right to terminate your access to our platform at any time and for any reason, without liability to you.
+                  <br/><br/>
+                  If you have any questions or concerns about these terms and conditions, please contact us at ciitplusplus@gmail.com.
+                </Typography>
+              </Box>
+            </Modal>
+
+
+
           </Box>
         </form>
       )}
